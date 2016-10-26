be50f0f3ac1848a732c3f53df626308cf5bc987a
Richard Gomes <rgomes.info@gmail.com>
2016-10-26 23:57:36 +0100

    Remove stretch-kde-minimal

--- stretch-kde-minimal/resources/vnc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-[[ ! -d /root/.vnc ]] && mkdir /root/.vnc
-
-echo password | /opt/TurboVNC/bin/vncpasswd -f -t
-/opt/TurboVNC/bin/vncserver :1 -geometry 1920x1280 -depth 24 && tail -F /root/.vnc/*.log
