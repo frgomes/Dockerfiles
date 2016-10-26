f3ca684494b567f491004d1e5d4056f403e5f60e
Richard Gomes <rgomes.info@gmail.com>
2016-10-26 22:58:06 +0100

    typo

--- sid-kde-minimal/build.sh
+++ sid-kde-minimal/build.sh
@@ -6,10 +6,10 @@ name=$(basename $(readlink -f $dir))
 
 tag=rgomes/${name}
 
-if [ !f ${dir}/resources/turbovnc_2.1_amd64.deb ] ;then
+if [ ! -f ${dir}/resources/turbovnc_2.1_amd64.deb ] ;then
   wget http://heanet.dl.sourceforge.net/project/turbovnc/2.1/turbovnc_2.1_amd64.deb -O ${dir}/resources/turbovnc_2.1_amd64.deb
 fi
-if [ !f ${dir}/resources/turbovnc_2.1_amd64.deb ] ;then
+if [ ! -f ${dir}/resources/turbovnc_2.1_amd64.deb ] ;then
   echo ERROR: Failed to download turbovnc_2.1_amd64.deb
 else
   docker build -t ${tag} $dir
